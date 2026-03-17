f96c8b364874157bd0c19683db082a8e01d0f508
Andrey <andreycrencef@gmail.com>
2026-03-16 21:09:23 -0300

    Criei o projeto/arquivo

---
+++ main.c
@@ -0,0 +1,10 @@
+//Biblioteca
+#include <stdio.h> 
+
+//Código 
+int main () { 
+    printf("Aluno: Andrey Crence Fernandes\n"); 
+    printf("RM: 573840\n"); 
+    printf ("Idade: 20\n"); 
+    return 0; 
+}
