c1385fc38f85c188eb5f47cebe6712ef24bfa7af
Fernando <labsfiap@fiap.com.br>
2026-03-23 08:02:08 -0300

    adicionei nome do aluno fernando

--- main.c
+++ main.c
@@ -6,5 +6,6 @@ int main () {
     printf("Aluno: Andrey Crence Fernandes\n");
     printf("RM: 573840\n");
     printf ("Idade: 20\n");
+    printf("Aluno: Fernando Bonfim Hoefle\n");
     return 0; 
 }
